91ce62dfff4a322f374df85a739f9ef4d307956b
Eman Abdullah <emanabdullah467@gmail.com>
2026-04-03 00:11:41 +0500

    inde.js some changes

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ root.render(
   <React.StrictMode>
     <AuthProvider>
       <CartProvider>
-        <Router>
-          <App />
-        </Router>
+        <App />
       </CartProvider>
     </AuthProvider>
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
