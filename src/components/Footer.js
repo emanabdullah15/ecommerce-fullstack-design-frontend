08987982828213ef22360d89fa6ac27abb1e04e4
Eman Abdullah <emanabdullah467@gmail.com>
2026-04-03 09:51:53 +0500

    remove unused things in frontend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { FlagIcon } from "react-flag-kit";
 import "../styles/Footer.css";
 
